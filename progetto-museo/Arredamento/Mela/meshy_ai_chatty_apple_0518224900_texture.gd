ce88cb8111b29a666d59458d67981538b682e15e
Scuola-smth <alessandro.sala1@leomail.it>
2026-05-19 23:10:06 +0200

    Sala: finito percorso con paletti

---
+++ progetto-museo/Arredamento/Mela/meshy_ai_chatty_apple_0518224900_texture.gd
@@ -0,0 +1,24 @@
+extends Node3D
+
+@export var titolo := "Mela"
+@export_multiline var descrizione := "Ciao, sono una mela!
+Tu puoi chiamarmi Branci"
+
+var player_near = false
+
+func _on_area_3d_body_entered(body):
+	if body.name == "DesktopPlayer":
+		player_near = true
+		body.show_prompt(true)
+
+
+func _on_area_3d_body_exited(body):
+	if body.name == "DesktopPlayer":
+		player_near = false
+		body.show_prompt(false)
+
+
+func _process(delta):
+	if player_near and Input.is_action_just_pressed("interact"):
+		var ui = get_tree().current_scene.get_node("UI")
+		ui.show_info(titolo, descrizione)
